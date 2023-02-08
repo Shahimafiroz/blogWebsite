1b2f179bd982457169bd1dfdff285c999d419ff3
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-02-08 21:25:01 +0530

    10

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home.ejs", { blog1: homeStartingContent });
-  console.log(postList);
+  res.render("home.ejs", { blog1: homeStartingContent, blogs: postList });
 });
 
 app.get("/about", function (req, res) {
@@ -38,17 +38,32 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  // console.log(req.body.inpTitle);
-  // console.log(req.body.inp);
   const newBlog = {
     Title: req.body.inpTitle,
     Content: req.body.inp,
   };
-  // console.log(newBlog);
   postList.push(newBlog);
-
   res.redirect("/");
 });
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  console.log(requestedTitle);
+  //
+  //
+  //
+  postList.forEach((cell) => {
+    const arrayTitle = _.lowerCase(cell.Title);
+    // console.log(arrayTitle);
+    if (requestedTitle === arrayTitle) {
+      console.log("Match Found!");
+    } else {
+      console.log("ooh nooo !");
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
